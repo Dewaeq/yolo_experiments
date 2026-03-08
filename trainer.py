727fb554b34ecc47eec97fd53c5f325fb04f3dbd
dewaeq <dewaeq@protonmail.com>
2026-03-08 21:59:11 +0100

    patience

--- trainer.py
+++ trainer.py
@@ -5,7 +5,11 @@ from ultralytics.models import YOLO
 def train_model(args):
     model = YOLO(args.model)
     model.train(
-        data=args.data, epochs=args.epochs, imgsz=args.resolution, project=args.project
+        data=args.data,
+        epochs=args.epochs,
+        imgsz=args.resolution,
+        project=args.project,
+        patience=args.patience,
     )
 
 
@@ -15,6 +19,12 @@ if __name__ == "__main__":
     )
     parser.add_argument("--model", type=str, default="yolo26n-seg")
     parser.add_argument("--epochs", type=int, default=50)
+    parser.add_argument(
+        "--patience",
+        type=int,
+        default=50,
+        help="Early Stopping if no improvement for this many epochs",
+    )
     parser.add_argument("--resolution", type=int, default=1024)
     parser.add_argument(
         "--data",
